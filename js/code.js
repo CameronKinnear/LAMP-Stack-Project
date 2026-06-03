dbf25f9c81b7eef73768a4cc16fb42dc009dc42b
Cameron Kinnear <cmkin402@gmail.com>
2026-06-03 12:43:18 -0400

    more login javascript and new page for contacts

--- js/code.js
+++ js/code.js
@@ -1,3 +1,20 @@
-function TryPassword() {
-    return document.getElementById('login-status').innerHTML = "Wrong username or password"
+function TryLogin() {
+    const USER = document.getElementById('user').value;
+    const PASS = document.getElementById('pass').value;
+
+    let validLogin = true;
+
+    // TODO: Check DB for username and password
+
+    if (validLogin) {
+        document.getElementById('login-status').style.color = 'green'
+        document.getElementById('login-status').innerHTML = "Valid username and password"
+        window.location.href = '/contacts.html'
+        return
+    } else {
+        document.getElementById('login-status').style.color = 'red'
+        document.getElementById('login-status').innerHTML = "Wrong username or password"
+        return
+    }
+
 }
