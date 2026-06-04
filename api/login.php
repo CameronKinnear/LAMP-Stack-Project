e1463c230c4d902ce9c8c98fb8e2b3f8ad2c9665
plaserd <cmkin402@gmail.com>
2026-06-04 10:38:35 -0400

    testing conn line with db

--- api/login.php
+++ api/login.php
@@ -4,6 +4,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
+$conn = new mysqli('camkin.xyz', 'root', 'Summer$R007', 'LampContactsManager')
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
