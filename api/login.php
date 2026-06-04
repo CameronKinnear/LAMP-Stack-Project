9613f8c0c815cdd442bf7fa2d4b508358cabb152
plaserd <cmkin402@gmail.com>
2026-06-04 10:42:54 -0400

    more db connection update

--- api/login.php
+++ api/login.php
@@ -4,7 +4,17 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli('camkin.xyz', 'root', 'Summer$R007', 'LampContactsManager')
+$servername = "localhost";
+$username = "root";
+$password = "Summer#R007";
+$dbname = "LampContactsManager";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
